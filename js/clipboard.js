be07cc32b5c1f7b311013552b1202dbdbea5ead8
sirochro <sirochro@MacBook-2.local>
2015-12-20 16:53:57 +0900

    Site updated: 2015-12-20 16:53:56

--- js/clipboard.js
+++ js/clipboard.js
@@ -1,19 +1,16 @@
 $(document).ready(function() {
-  console.log("zero");
+  //console.log("zero");
   var btn = $('.zc');
-
   if ( !isMobile() ){
     try {
-      console.log("zero try");
       if(!new ActiveXObject('ShockwaveFlash.ShockwaveFlash')) {
         console.log("zero try -> display:none");
-        btn.style.display = "none";
+        btn.css('display', 'none');
       }
     } catch(e) {
-      console.log("zero catch");
       if(navigator.mimeTypes ["application/x-shockwave-flash"] === undefined) {
         console.log("zero catch -> display:none");
-        btn.style.display = "none";
+        btn.css('display', 'none');
       }
     }
     var clip = new ZeroClipboard($(".zc"));
@@ -21,7 +18,6 @@ $(document).ready(function() {
 
     });
   } else {
-    console.log("isMobile() -> display:none");
-    btn.style.display = "none";
+    btn.css('display', 'none');
   }
 });
